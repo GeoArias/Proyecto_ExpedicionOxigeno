e74000902432d0ff5d03515f337cd1c1b95ffa93
Geovanny Arias <geoas121cr@gmail.com>
2025-07-04 21:13:33 -0600

    Modificación a clases de Bookings

--- Proyecto_ExpedicionOxigeno/Controllers/ReservasController.cs
+++ Proyecto_ExpedicionOxigeno/Controllers/ReservasController.cs
@@ -10,15 +10,19 @@ using System.Web.Mvc;
 
 namespace Proyecto_ExpedicionOxigeno.Controllers
 {
-    public class ReservaController : Controller
-    { 
-        
-        // GET: Reserva
+    public class ReservasController : Controller
+    {
+
+        // GET: Reservas
         public async Task<ActionResult> Index()
         {
             // Cargar todos los servicios disponibles
             List<BookingService> services = await MSBookings_Actions.Get_MSBookingsServices();
             ViewBag.Services = services;
+
+            BookingStaffAvailabilityCollectionResponse  staffAvailability = await MSBookings_Actions.Get_MSBookingsStaffAvailability(services[0].StaffMemberIds, DateTime.Today, DateTime.UtcNow.AddDays(30));
+            ViewBag.StaffAvailability = staffAvailability;
+
             return View();
         }
     }
---
+++ Proyecto_ExpedicionOxigeno/Models/BookingStaffAvailability.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Graph.Models;
+using System;
+using System.Collections.Generic;
+
+// The main response model
+public class BookingStaffAvailabilityCollectionResponse
+{
+    public List<BookingStaffAvailability> Value { get; set; }
+}
+
+// Individual staff availability
+public class BookingStaffAvailability
+{
+    public string StaffId { get; set; }
+    public List<BookingStaffAvailabilityItem> AvailabilityItems { get; set; }
+}
+
+// Specific time slots
+public class BookingStaffAvailabilityItem
+{
+    public DateTimeOffset StartDateTime { get; set; }
+    public DateTimeOffset EndDateTime { get; set; }
+    public string Status { get; set; }
+}
